69073ee9395b5be90b00f792083c2b5922cfdaa0
Jesus De La Rosa <budderlod@gmail.com>
2025-04-11 02:24:43 -0400

    hp bar

---
+++ Assets/UI/HPBarUI.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarUI : MonoBehaviour
+{
+    public PlayerController owner;
+    public GameObject segment;
+    public List<GameObject> segments;
+    public Transform fill;
+    public Slider slider;
+
+    void Start()
+    {
+        while(segments.Count != owner.maxHp) {
+            AddSegment();
+        }
+    }
+
+    void Update()
+    {
+        slider.value = owner.hp;
+        //update the amount of segments if the maxHp value changes from like items or shit
+        if(slider.maxValue < owner.maxHp) {
+            AddSegment();
+            slider.maxValue++;
+        }
+        else if(slider.maxValue > owner.maxHp) {
+            RemoveSegment();
+            slider.maxValue--;
+        }
+    }
+
+    public void AddSegment() {
+        segments.Add(Instantiate(segment, fill));
+    }
+
+    public void RemoveSegment() {
+        Destroy(segments.Last());
+        segments.RemoveAt(segments.Count-1);
+    }
+
+    public void UpdateSegment(float value) {
+        int sliderValue = (int)value;
+        Debug.Log(sliderValue + " " + segments.Count);
+
+        if(sliderValue < segments.Count) {
+            RemoveSegment();
+        }
+        else if(sliderValue > segments.Count) {
+            AddSegment();
+        }
+    }
+}
